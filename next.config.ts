804fd6a8ddfdd2521912a42be6ab4be852a128d6
segalv <anaclarasvp@usp.br>
2025-06-23 09:17:07 -0300

    Remove basePath e assetPrefix para corrigir 404 na Vercel

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: '/biblioteca_cursinho_frontend',
-  assetPrefix: '/biblioteca_cursinho_frontend/',
+  //basePath: '/biblioteca_cursinho_frontend',
+  //assetPrefix: '/biblioteca_cursinho_frontend/',
 };
 
 export default nextConfig;
