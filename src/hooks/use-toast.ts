3738da51d9c61a82d0a1e271d242e5cc94930f19
Anemaygi <gianemayumisan@gmail.com>
2025-09-23 12:47:05 -0400

    remove parte do codigo

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -32,26 +32,6 @@ function genId() {
   return count.toString()
 }
 
-type ActionType = typeof actionTypes
-
-type Action =
-  | {
-      type: ActionType["ADD_TOAST"]
-      toast: ToasterToast
-    }
-  | {
-      type: ActionType["UPDATE_TOAST"]
-      toast: Partial<ToasterToast>
-    }
-  | {
-      type: ActionType["DISMISS_TOAST"]
-      toastId?: ToasterToast["id"]
-    }
-  | {
-      type: ActionType["REMOVE_TOAST"]
-      toastId?: ToasterToast["id"]
-    }
-
 interface State {
   toasts: ToasterToast[]
 }
